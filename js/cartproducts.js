146d22dff05d8c608c08ffe76a885a7ba7e89453
L.A360 <lat.tif8605@gmail.com>
2024-03-21 16:51:18 +0300

    the functionality of adding products to the shop page directly from the form was enabled

--- js/cartproducts.js
+++ js/cartproducts.js
@@ -1,3 +1,5 @@
+shopProducts = JSON.parse(localStorage.getItem("shopProducts"));
+
 const cartIcon = document.querySelector(".shop-cart");
 const cartSection = document.querySelector(".cart-section");
 const cartContainer = document.querySelector(".cart-container");
@@ -10,25 +12,27 @@ let cartItems=[];
 
 
 cartIcon.addEventListener("click", function () {
-    cartContainer.classList.toggle("show-cart");
-  });
+  // console.log()
+  cartContainer.classList.toggle("show-cart");
+});
 
-  if(cartItems.length===0){
-    cartSection.innerHTML=`
-    <h3> No products added to cart</h3>`
-  }
+if(cartItems.length===0){
+  cartSection.innerHTML=`
+  <h3> No products added to cart</h3>`
+}
 
-  function updateCartItems() {
-    cartSection.innerHTML = "";
+function updateCartItems() {
+  cartSection.innerHTML = "";
 
     cartItems.map((singleProduct) => {
       let cartProduct = document.createElement("div");
       cartProduct.classList.add("shoped-product");
       cartProduct.innerHTML = `
-          <img src=${singleProduct.image} width="100px">
-          <h3>${singleProduct.name}</h3>
+          <img src=${singleProduct.image}">
+          <h5>${singleProduct.name}</h5>
           <p>${singleProduct.description}</p>
           <p >Ksh. <span class="single-price">${singleProduct.price}</span></p>
+          
           <div class="cart-tally">
           <button class="decreament">-</button>
           <p class="quantity">1</p>
@@ -36,52 +40,54 @@ cartIcon.addEventListener("click", function () {
           </div>
           <p class="newPrice">${singleProduct.price}</p>
           `;
-        
+          
       cartSection.append(cartProduct);
   
   
     });
-    if(cartItems.length===0){
-      cartSection.innerHTML= `
-      <h3>Nothing added to cart</h3>
-      `
-      cartTotal.style.display="none"
-    }
+  //  
   updatePrices()
   }
 
 
 
   for (let p = 0; p < btn.length; p++) {
+
     btn[p].addEventListener("click", function (e) {
-     
-     
-  
-      if (e.target.textContent.toLowerCase() === "add to cart") {
-        let singleProduct={
-          image:singleProduct.image,
-          name:singleProduct.name,
-          descrption:singleProduct.descrption,
-         price: singleProduct.price,
+
+
+      let singleProductImage = e.target.parentElement.children[0].getAttribute("src");
+      let singleProductName = e.target.parentElement.children[1].textContent;
+      let singleProductDescrption = e.target.parentElement.children[2].textContent;
+      let singleProductPrice = e.target.parentElement.children[3].textContent;
+      // console.log(e)
+   if (e.target.textContent.toLowerCase() === "add to cart") {
+        const singleProduct={
+          image:singleProductImage,
+          name:singleProductName,
+          description:singleProductDescrption,
+          price:singleProductPrice,
         }
+        
        
         
-        // console.log(singleProduct);
+        console.log(singleProduct);
         // console.log(e);
         e.target.style.background = "red";
         e.target.textContent = "Remove from cart";
         cartItems.push(singleProduct);
         count.textContent = cartItems.length;
   
-        // updateCartItems();
-      } else {
+        updateCartItems();
+      } 
+      else {
         const indexToRemove = cartItems.findIndex(
-          (singleProduct) => singleProduct.name === productName.value
+          (singleProduct) => singleProduct.name === singleProductName.value
         );
-        console.log(indexToRemove);
+        // console.log(indexToRemove);
         cartItems.splice(indexToRemove, 1);
         count.textContent = cartItems.length;
-        e.target.style.background = "black";
+        e.target.style.background = "gray";
         e.target.textContent = "Add to cart";
         updateCartItems();
       }
@@ -89,17 +95,17 @@ cartIcon.addEventListener("click", function () {
   }
 
   cartSection.addEventListener("click", function (e) {
-    // console.log(e)
+    
     if (e.target.classList.contains("increament")) {
       const currentQuantityElement = e.target.previousElementSibling;
       currentQuantityElement.textContent++;
-      // console.log("adding items")
+      
     } else if (e.target.classList.contains("decreament")) {
       const nextQuantityElement = e.target.nextElementSibling;
       if (nextQuantityElement.textContent > 1) {
         nextQuantityElement.textContent--;
       }
-      // // console.log("deleting items")
+      
     }
   
     updatePrices();
@@ -109,13 +115,13 @@ cartIcon.addEventListener("click", function () {
 
   function updatePrices() {
     let totalPrice=0;
-    // console.log(ggg)
+    
     const cartItemProducts = document.querySelectorAll(".shoped-product");
-     console.log(cartItemProducts)
+    //  console.log(cartItemProducts)
     for (let p = 0; p < cartItemProducts.length; p++) {
       let itemElement = cartItemProducts[p];
     //   console.log(itemElement)
-      const itemName = itemElement.querySelector("h3").textContent;
+      const itemName = itemElement.querySelector("h5").textContent;
     //   console.log(itemName);
      let item= cartItems.find(element=>element.name===itemName)
   
@@ -124,7 +130,7 @@ cartIcon.addEventListener("click", function () {
       console.log(quantity)
       console.log(typeof(quantity))
       let price=parseInt(itemElement.querySelector(".single-price").textContent)
-      console.log(price)
+      // console.log(price)
       let addedPrice=quantity*price
       itemElement.querySelector(".newPrice").textContent=addedPrice
       totalPrice+=addedPrice
--- js/shop.js
+++ js/shop.js
@@ -3,14 +3,14 @@ shopProducts = JSON.parse(localStorage.getItem("shopProducts"));
 const productsDiv=document.querySelector(".home__products");
 // shopProducts.push(singleProduct);
 // localStorage.setItem("shopProducts", JSON.stringify(shopProducts));
-
+// let btn=document.querySelectorAll(".cart__btn");
 
 function upadateShopItems() {
   // const singleProduct = {};
     // productsDiv.innerHTML="";
     shopProducts.map((singleProduct) => {
       let shopItem = document.createElement("div");
-      shopItem.classList.add(".display__products");
+      shopItem.classList.add(".shoped-product");
       console.log(singleProduct)
 
       shopItem.innerHTML = `
@@ -19,7 +19,7 @@ function upadateShopItems() {
           <h5>${singleProduct.name}</h5>
           <p>${singleProduct.descrption}</p>
           <p>Ksh.${singleProduct.price}</p>
-          <button class="cart__btn">Add to cart</button>
+          <button class="add__btn">Add to cart</button>
       `;
 
     productsDiv.append(shopItem)
